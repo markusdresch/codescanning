13c9421f5aee5154781272be5ec97f7f8e021cf1
Markus Dresch <markus.dresch@itelio.com>
2022-02-22 09:23:50 +0100

    initial test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private int _x;
+
+        public int X
+        {
+            get { return _x; }
+            // no setter
+            set { }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // no server cert validation
+            ServicePointManager.DefaultConnectionLimit = 100;
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            // tempfile instead of randomfile
+            var tempFileName = Path.GetTempFileName();
+
+
+            // endless loop
+            var i = 0;
+            while (true)
+            {
+                i++;
+            }
+
+        }
+    }
+}
