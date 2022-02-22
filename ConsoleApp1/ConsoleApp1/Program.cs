6a9cec506460505e41c9b9286b0448aa26dc065d
Markus Dresch <markus.dresch@itelio.com>
2022-02-22 10:24:53 +0100

    more vulnerabilities

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
 
 namespace ConsoleApp1
 {
@@ -31,6 +34,25 @@ namespace ConsoleApp1
 
             // tempfile instead of randomfile
             var tempFileName = Path.GetTempFileName();
+
+
+            // command injection
+            var p = new Process();
+            p.StartInfo.FileName = "exportLegacy.exe";
+            p.StartInfo.Arguments = " -user " + args[0] + " -role user";
+            p.Start();
+
+
+            // xpath injection
+            var doc = new XmlDocument { XmlResolver = null };
+            doc.Load("/config.xml");
+            var results = doc.SelectNodes("/Config/Devices/Device[id='" + args[0] + "']");
+
+
+            // weak hash
+            var hashProvider = new SHA1CryptoServiceProvider();
+            var hash = hashProvider.ComputeHash(Encoding.UTF8.GetBytes(args[0]));
+
 
 
             // endless loop
